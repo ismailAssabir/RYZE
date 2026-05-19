7505905253906a992797fdea0e22cf99ff7ed62d
ismail <ismailassabir02@gmail.com>
2026-05-19 21:59:07 +0100

    fix: prevent empty environment cache variables from breaking Laravel bootstrap on Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,13 @@
 <?php
 
+// Fix for Railway deployment: prevent empty environment cache variables from resolving to the base path.
+// This prevents the "require(/app): Failed to open stream" error.
+foreach (['APP_CONFIG_CACHE', 'APP_EVENTS_CACHE', 'APP_PACKAGES_CACHE', 'APP_ROUTES_CACHE', 'APP_SERVICES_CACHE'] as $__key) {
+    if (isset($_ENV[$__key]) && $_ENV[$__key] === '') unset($_ENV[$__key]);
+    if (isset($_SERVER[$__key]) && $_SERVER[$__key] === '') unset($_SERVER[$__key]);
+    if (getenv($__key) === '') putenv($__key);
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
